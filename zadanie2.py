25a2c14b4e3692592ebd37e0d4f671b778f9b78a
Artem Lisenkov <radeon-1@mail.ru>
2017-11-20 13:21:14 +0300

    lesson2

---
+++ zadanie2.py
@@ -0,0 +1,51 @@
+
+
+
+
+first_str = str(input('введите первую строку: '))
+second_str = str(input('введите вторую строку: '))
+dlina1 = (len(first_str))
+dlinа2 = (len(second_str))
+#print(dlina1)
+#print(dlinа2)
+
+
+
+def compare_srt(dlina1, dlinа2):
+    if first_str == second_str:
+       # return print("равны")
+       return 1
+        
+
+    elif dlina1 > dlinа2:
+       # return print("1й больше")
+       return 2
+        
+    
+    elif first_str != second_str and second_str == "learn":
+       # return print("не равны")
+       return 3
+        
+
+
+compare_srt(dlina1, dlinа2)
+
+
+
+
+
+
+            
+ 
+
+
+
+#print(first_str)
+#print(second_str)
+
+
+
+
+
+
+
